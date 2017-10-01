813d6bcaaca69c83cb6b28eb65e1094ab8005fd0
gabeduke <gabeduke@gmail.com>
2017-09-30 23:37:29 -0400

    refactor to build own image and deprecate dcao

--- build_stack.sh
+++ build_stack.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # pull in external docker-compose files
-git clone https://github.com/gabeduke/opsdroid-jenkins:docker-compose modules/docker-compose
+git clone -b docker-compose https://github.com/gabeduke/opsdroid-jenkins config/modules/opsdroid-jenkins
 
 # merge docker-compose files
 docker-compose -f docker-compose-core.yaml -f config/modules/opsdroid-jenkins/docker-compose.yaml config > docker-compose.yaml
