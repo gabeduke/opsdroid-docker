78af6b2cadfaa8e1292fc37de1eb44a52058beab
gabeduke <gabeduke@gmail.com>
2017-10-01 00:09:40 -0400

    formatting

--- build_stack.sh
+++ build_stack.sh
@@ -1,10 +1,16 @@
 #!/usr/bin/env bash
 
 # pull in external docker-compose files
-git clone -b docker-compose https://github.com/gabeduke/opsdroid-jenkins config/modules/opsdroid-jenkins
+git clone \
+    -b docker-compose \
+    https://github.com/gabeduke/opsdroid-jenkins \
+    config/modules/opsdroid-jenkins
 
 # merge docker-compose files
-docker-compose -f docker-compose-core.yaml -f config/modules/opsdroid-jenkins/docker-compose.yaml config > docker-compose.yaml
+docker-compose \
+    -f docker-compose-core.yaml \
+    -f config/modules/opsdroid-jenkins/docker-compose.yaml \
+    config > docker-compose.yaml
 
 # Run opsdroid docker-compose cluster
 docker-compose up -d --build
